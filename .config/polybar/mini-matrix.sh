e2d8df17ea05f0520ffb1eb8db732141466d7266
SkwalExe <skwal.net@gmail.com>
2022-01-09 07:52:59 +0100

    added config files

---
+++ .config/polybar/mini-matrix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo $(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))$(($RANDOM%2))
---
+++ .config/polybar/start.sh
@@ -0,0 +1,6 @@
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar main &
+
