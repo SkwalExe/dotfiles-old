210a9ed84f43b9bead01829d2f04367926be77f7
SkwalExe <support@skwal.net>
2022-03-03 18:12:41 +0100

    Added missing polybar build dependencies

--- install.sh
+++ install.sh
@@ -200,7 +200,7 @@ if [[ "${confirmation,,}" =~ ^(a|b)$ ]]; then
         printf "${blue}[ i ] : Installing build dependencies...\n"
 
 
-        if ! sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev ; then
+        if ! sudo apt install meson uthash-dev libconfig-dev libgl-dev libdbus-1-dev libev-dev libx11-xcb-dev libxcb-damage0-dev libxcb-sync-dev libxcb-xinerama0-dev libxcb-present-dev libxcb-glx0-dev build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev ; then
           printf "${red}[ x ] : An error occured installing build dependencies with apt !\n"
           exit 1
         fi
